b197c15d42deeb515bd5701058800151cb7469d3
vanineku <shingethu0308@gmail.com>
2024-10-17 00:14:34 +0900

    [Add] index2

---
+++ ToDoApp2.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import ToDoInput from"./ToDoInput.vue";
+  import ToDoListView from "./ToDoListView.vue";
+
+</script>
+
+<template>  
+  <div class="todoApp">
+    <ToDoInput></ToDoInput>
+    <ToDoListView></ToDoListView>
+  </div>
+</template>
